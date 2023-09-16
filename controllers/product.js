63d48e410fb8e2c14eea03f71301ccf2030b00b6
amitozs999 <ekas0001@gmail.com>
2023-09-16 16:23:02 +0530

    sall products and update product page done

--- controllers/product.js
+++ controllers/product.js
@@ -18,6 +18,54 @@ exports.create = async (req, res) => {
 };
 
 exports.read = async (req, res) => {
-  let products = await Product.find({});
+  const product = await Product.findOne({ slug: req.params.slug })
+    .populate("category")
+    .populate("subs")
+    .exec();
+  res.json(product);
+};
+
+exports.listAll = async (req, res) => {
+  let products = await Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate("category") //response me cat ke fields bhi dal dega
+    .populate("subs")
+    .sort([["createdAt", "desc"]])
+    .exec();
   res.json(products);
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Product.findOneAndRemove({
+      slug: req.params.slug,
+    }).exec();
+
+    res.json(deleted);
+  } catch (err) {
+    console.log(err);
+    return res.staus(400).send("Product delete failed");
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title); //if titile in req body not null slugify it add in slug value
+    }
+
+    const updated = await Product.findOneAndUpdate(
+      { slug: req.params.slug }, //find that slug
+      req.body, //jo value ayi he body me i.e new updated value pass tha
+      { new: true } //old vali remove
+    ).exec();
+
+    res.json(updated);
+  } catch (err) {
+    console.log("PRODUCT UPDATE ERROR ----> ", err);
+    // return res.status(400).send("Product update failed");
+    res.status(400).json({
+      err: err.message,
+    });
+  }
 };
--- routes/product.js
+++ routes/product.js
@@ -7,8 +7,18 @@ const { authCheck, adminCheck } = require("../middlewares/auth");
 // controller
 const { create, read } = require("../controllers/product");
 
+const { listAll, remove, update } = require("../controllers/product");
+
 // routes
-router.get("/products", read);
+
+//router.get("/products", read);   using below now
+router.get("/products/:count", listAll); // products/100
+
+router.get("/product/:slug", read);
+
 router.post("/product", authCheck, adminCheck, create);
+
+router.delete("/product/:slug", authCheck, adminCheck, remove);
+router.put("/product/:slug", authCheck, adminCheck, update);
 
 module.exports = router;
